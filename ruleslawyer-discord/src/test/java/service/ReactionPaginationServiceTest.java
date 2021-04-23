160c4242e707b470f75818d0a78152ecadeeec7f
elainecao93 <elainecao93@users.noreply.github.com>
2021-04-23 12:19:10 -0600

    1.9.5

--- ruleslawyer-discord/src/test/java/service/ReactionPaginationServiceTest.java
+++ ruleslawyer-discord/src/test/java/service/ReactionPaginationServiceTest.java
@@ -7,7 +7,7 @@ import search.DiscordRuleSearchService;
 import search.contract.DiscordSearchRequest;
 import service.reaction_pagination.ReactionPaginationService;
 
-import static contract.rules.enums.RuleRequestCategory.PAPER;
+import static contract.rules.enums.RuleRequestCategory.ANY_RULE_TYPE;
 import static contract.rules.enums.RuleSource.ANY_DOCUMENT;
 import static contract.rules.enums.RuleSource.CR;
 import static java.util.Arrays.asList;
@@ -30,11 +30,11 @@ public class ReactionPaginationServiceTest {
     @Test
     public void ParseEmbeddedRequest() {
         DiscordSearchRequest nonspecificSearchRequest = reactionPaginationService.getSearchRequestFromEmbed("mana/symbol", "Requested by: Elaine | page 1 of 10 | Use arrow reactions for pagination");
-        DiscordSearchRequest expectedNonspecificResult = new DiscordSearchRequest("Elaine", "TO BE IMPLEMENTED", asList("mana", "symbol"), ANY_DOCUMENT, 1, PAPER);
+        DiscordSearchRequest expectedNonspecificResult = new DiscordSearchRequest("Elaine", "TO BE IMPLEMENTED", asList("mana", "symbol"), ANY_DOCUMENT, 1, ANY_RULE_TYPE);
         assertThat(nonspecificSearchRequest, is(expectedNonspecificResult));
 
         DiscordSearchRequest specificSearchRequest = reactionPaginationService.getSearchRequestFromEmbed("CR | mana/symbol\n", "Requested by: Elaine | page 1 of 10 | Use arrow reactions for pagination");
-        DiscordSearchRequest expectedSpecificResult = new DiscordSearchRequest("Elaine", "TO BE IMPLEMENTED", asList("mana", "symbol"), CR, 1, PAPER);
+        DiscordSearchRequest expectedSpecificResult = new DiscordSearchRequest("Elaine", "TO BE IMPLEMENTED", asList("mana", "symbol"), CR, 1, ANY_RULE_TYPE);
         assertThat(specificSearchRequest, is(expectedSpecificResult));
     }
 }
